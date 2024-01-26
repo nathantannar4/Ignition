22482ae208235692e053282285e04c156b6fa66b
Nathan Tannar <nathantannar4@gmail.com>
2024-01-25 16:23:14 -0800

    1.0.2

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .macCatalyst(.v13),
         .tvOS(.v13),
         .watchOS(.v6),
-//        .visionOS(.v1),
+        .visionOS(.v1),
     ],
     products: [
         .library(
