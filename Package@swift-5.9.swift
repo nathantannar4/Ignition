f83f4b3c43e2842f5b6f1555b741a7231d1fe993
Nathan Tannar <nathantannar4@gmail.com>
2025-08-07 11:57:30 -0700

    2.1.0

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "Ignition",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .macCatalyst(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .visionOS(.v1),
+    ],
+    products: [
+        .library(
+            name: "Ignition",
+            targets: ["Ignition"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/nathantannar4/Engine", from: "2.1.14"),
+    ],
+    targets: [
+        .target(
+            name: "Ignition",
+            dependencies: [
+                "Engine"
+            ]
+        )
+    ]
+)
